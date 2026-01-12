15a4a6ecc64a3cbe785d1c7826bfc2fcfcc03aaa
bargin <912465467@qq.com>
2026-01-12 16:15:05 +0800

    fix: improve judge-interval with better typing and documentation

--- src/helpers/judge-interval.ts
+++ src/helpers/judge-interval.ts
@@ -1,25 +1,65 @@
-export function getInterval(data: any) {
+/**
+ * Time constants in milliseconds
+ */
+const MS_IN_DAY = 24 * 3600 * 1000;
+const MS_IN_MONTH = 30 * MS_IN_DAY;
+const MS_IN_YEAR = 365 * MS_IN_DAY;
+
+/**
+ * Time data point type
+ */
+type TimeDataPoint = [string, number];
+
+/**
+ * Chart instance type with minimum required methods
+ */
+interface ChartInstance {
+  on(event: string, callback: (params: any) => void): void;
+  getOption(): any;
+  setOption(option: any): void;
+}
+
+/**
+ * Get time interval information from data
+ * @param data Array of time data points
+ * @returns Object containing time length and minimum interval
+ */
+export function getInterval(data: TimeDataPoint[]): { timeLength: number; minInterval: number } {
+  if (!data || data.length === 0) {
+    return { timeLength: 0, minInterval: MS_IN_MONTH };
+  }
+
+  // Extract year from the first and last data points
   const startTime = Number(data[0][0].split('-')[0]);
   const endTime = Number(data[data.length - 1][0].split('-')[0]);
   const timeLength = endTime - startTime;
-  const minInterval = timeLength > 2 ? 365 * 24 * 3600 * 1000 : 30 * 3600 * 24 * 1000;
+
+  // Determine minimum interval based on time length
+  const minInterval = timeLength > 2 ? MS_IN_YEAR : MS_IN_MONTH;
+
   return { timeLength, minInterval };
 }
 
-export function judgeInterval(instance: any, timeLength: number) {
+/**
+ * Judge and set appropriate interval for chart based on time length
+ * @param instance Chart instance
+ * @param timeLength Time length in years
+ */
+export function judgeInterval(instance: ChartInstance, timeLength: number): void {
   if (timeLength > 2) {
     instance.on('dataZoom', (params: any) => {
-      let chartOption = instance.getOption() as {
-        xAxis: { minInterval?: any }[];
-      };
-      const startValue = params.batch[0].start;
-      const endValue = params.batch[0].end;
-      let minInterval: number;
-      if (startValue == 0 && endValue == 100) {
-        minInterval = 365 * 24 * 3600 * 1000;
-      } else {
-        minInterval = 30 * 24 * 3600 * 1000;
+      const chartOption = instance.getOption();
+      if (!chartOption || !chartOption.xAxis || !chartOption.xAxis[0]) {
+        return;
       }
+
+      const startValue = params.batch?.[0]?.start;
+      const endValue = params.batch?.[0]?.end;
+
+      // Set minimum interval based on zoom level
+      // If fully zoomed out (0-100), use yearly interval, otherwise use monthly interval
+      const minInterval = startValue === 0 && endValue === 100 ? MS_IN_YEAR : MS_IN_MONTH;
+
       chartOption.xAxis[0].minInterval = minInterval;
       instance.setOption(chartOption);
     });
