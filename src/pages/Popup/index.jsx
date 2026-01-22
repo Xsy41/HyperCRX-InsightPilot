d6d73ef2e16fea8ba6bb78d97f16bd99121f3e3a
wangyantong2000 <18745996292@163.com>
2026-01-22 22:28:44 +0800

    deleted

--- src/pages/Popup/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Popup from './Popup';
-createRoot(window.document.querySelector('#app-container')).render(<Popup />);
-
-if (module.hot) module.hot.accept();
